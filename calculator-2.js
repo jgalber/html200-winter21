d62a61e4d6d5181db61b9f25e8e6bf2dae146145
jgalber <722751739+jgalber@users.noreply.github.com>
2021-01-22 19:57:07 -0800

    added extra calculator

---
+++ calculator-2.js
@@ -0,0 +1,51 @@
+// variables
+let opOne = 10;
+let opTwo = 35;
+let opThree = 5;
+let opFour = 2;
+
+// addition
+let sumResult = opOne + opTwo;
+console.log(sumResult);
+// 45
+
+// substraction
+let subsResult = opOne - opThree;
+console.log(subsResult); 
+// 5
+
+// division
+let divResult = opTwo / opFour;
+console.log(divResult);
+// 17.5
+
+// multiplication
+let mulResult = opOne * opThree;
+console.log(mulResult);
+// 50
+
+// modulo
+let modResult = opThree % opFour;
+console.log(modResult);
+// 1
+
+// exponentiation
+let expResult = opThree**2;
+console.log(expResult);
+// 25
+
+// incrementation (by one)
+let incResult = ++opFour;
+console.log(incResult);
+// 3
+
+// decrement (by one)
+let decResult = --opOne;
+console.log(decResult);
+// 9
+
+// converting a negative value
+let negResult = -opThree;
+console.log(negResult);
+// -5
+

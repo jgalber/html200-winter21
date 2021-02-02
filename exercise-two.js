5f1e757becb71034eb8a207d95d0190826f76b76
jgalber <72251739+jgalber@users.noreply.github.com>
2021-02-01 19:30:32 -0800

    exercise-two updated

---
+++ exercise-two.js
@@ -0,0 +1,10 @@
+// Exercise 2 complete
+const arr = ['a', 'group', 'of', 'gypsies', 'are', 'dancing', 'flamenco'];
+// Display only the third word
+  console.log(arr[2]); 
+// Join the string in one sentence
+let str = arr.join(' ');
+  console.log(str);
+// Add the period at the end of the sentence
+let str2 = str.concat('.');
+  console.log(str2);

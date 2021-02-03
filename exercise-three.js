4ec169ffcf41d4164c3f14a499473f3ac9aed9b6
jgalber <722751739+jgalber@users.noreply.github.com>
2021-02-02 18:38:59 -0800

    exercise three js

---
+++ exercise-three.js
@@ -0,0 +1,14 @@
+// Exercise 03
+let arr = [['1', '2', '3', '4','5'], ['apples', 'bananas', 'berries', 'pears', 'watermelon']];
+    console.log(arr[0][0]+', '+ arr[1][0]);
+    console.log(arr[0][1]+', '+ arr[1][1]);
+    console.log(arr[0][2]+', '+ arr[1][2]);
+    console.log(arr[0][3]+', '+ arr[1][3]);
+    console.log(arr[0][4]+', '+ arr[1][4]);
+
+// Exercise 03 with 'for' loop
+const southCastile = ['1, Toledo', '2, Ciudad Real', '3, Albacete', '4, Cuenca', '5, Guadalajara']; 
+    // console.log(southCastile);
+    for (let x = 0; x < southCastile.length; x++) {
+    console.log(southCastile[x]);
+}

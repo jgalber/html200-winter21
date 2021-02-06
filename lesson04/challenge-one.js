27ddab19c542fa3521632b959158f4dcd14479f8
jgalber <722751739+jgalber@users.noreply.github.com>
2021-02-05 16:53:23 -0800

    assignment challenge one

---
+++ lesson04/challenge-one.js
@@ -0,0 +1,9 @@
+function square() {
+    let input = prompt('Enter a number');
+    let numberToSquare = Number(input);
+    let result = 0;
+
+    result = Number(input) * Number(input);
+
+    alert(result);
+}
